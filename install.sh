8e4bf50e59dff3385e86d3ee371ddc690c4615d8
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-26 16:56:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-sudo pacman -Syu base-devel xorg-server xorg-xinit i3 ntfs-3g git nano rofi polybar dunst blueman bluez bluez-utils kitty nautilus nitrogen firefox picom neofetch
+sudo pacman -Syu base-devel xorg-server xorg-xinit i3 sddm ntfs-3g git nano rofi polybar dunst blueman bluez bluez-utils kitty nautilus nitrogen firefox picom neofetch
 
+sudo systemctl enable --now bluetooth.service
+bluetoothctl scan on
 echo "xrandr --output HDMI-1 --right-of DP-1" | sudo tee -a /etc/X11/xinit/xinitrc.d/51-display-settings.sh
 echo "nitrogen --set-zoom --random ~/wallpapers --head=0 && nitrogen --set-zoom-fill --random ~/wallpapers --head=1" | sudo tee -a /etc/X11/xinit/xinitrc.d/52-wallpaper-settings.sh
 
@@ -10,3 +12,7 @@ sudo pacman -S --needed base-devel git && git clone https://aur.archlinux.org/ya
 yay -Syu plex-media-server spotify jetbrains-toolbox
 
 cp -a config/. ~/.config
+
+# sort xinitrc
+
+startx
