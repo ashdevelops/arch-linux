578825eead7a254419e5cb6192d538125d07f31c
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-26 16:24:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-sudo pacman -Syu base-devel xorg-server xorg-xinit i3 ntfs-3g git nano rofi polybar dunst bluez bluez-utils kitty nautilus
+sudo pacman -Syu base-devel xorg-server xorg-xinit i3 ntfs-3g git nano rofi polybar dunst bluez bluez-utils kitty nautilus nitrogen
 
 echo "xrandr --output HDMI-1 --right-of DP-1" | sudo tee -a /etc/X11/xinit/xinitrc.d/51-display-settings.sh
+echo "" | sudo tee -a /etc/X11/xinit/xinitrc.d/52-wallpaper-settings.sh
 
 echo "UUID=46BA9DEABA9DD6B1       /mnt/other-drive	ntfs	defaults,noatime,rw,nofail 0 3" | sudo tee -a /etc/fstab
 systemctl daemon-reload
