7011e9b7354dee4a2d2ed9c05bc15e0d31b3dab2
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-26 17:25:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ bluetoothctl scan on
 echo "xrandr --output HDMI-1 --right-of DP-1" | sudo tee -a /etc/X11/xinit/xinitrc.d/51-display-settings.sh
 echo "nitrogen --set-zoom --random ~/wallpapers --head=0 && nitrogen --set-zoom-fill --random ~/wallpapers --head=1" | sudo tee -a /etc/X11/xinit/xinitrc.d/52-wallpaper-settings.sh
 
+sudo chmod +x /etc/X11/xinit/xinitrc.d/51-display-settings.sh
+sudo chmod +x /etc/X11/xinit/xinitrc.d/52-wallpaper-settings.sh
+
 echo "UUID=46BA9DEABA9DD6B1       /mnt/other-drive	ntfs	defaults,noatime,rw,nofail 0 3" | sudo tee -a /etc/fstab
 systemctl daemon-reload
 
@@ -16,8 +19,6 @@ cp -a config/. ~/.config
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 sed -n '/twm &/q;p' ~/.xinitrc
 echo "exec i3" > ~/.xinitrc
-
-mkdir ~/.config/picom && cp /etc/xdg/picom.conf ~/.config/picom/picom.conf
 
 mkdir ~/scripts
 
