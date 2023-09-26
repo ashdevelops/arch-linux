d443d2eac2dc2339437e823af8c440602a9053f5
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-26 16:19:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syu ntfs-3g git nano rofi polybar dunst bluez bluez-utils kitty
+sudo pacman -Syu base-devel xorg-server xorg-xinit i3 ntfs-3g git nano rofi polybar dunst bluez bluez-utils kitty
 
 echo "UUID=46BA9DEABA9DD6B1       /mnt/other-drive	ntfs	defaults,noatime,rw,nofail 0 3" | sudo tee -a /etc/fstab
 
