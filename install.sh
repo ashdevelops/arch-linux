61a9115ef98bc76758111496abbb11d62230ba3e
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-26 16:21:15 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo pacman -Syu base-devel xorg-server xorg-xinit i3 ntfs-3g git nano rofi polybar dunst bluez bluez-utils kitty
 
 echo "UUID=46BA9DEABA9DD6B1       /mnt/other-drive	ntfs	defaults,noatime,rw,nofail 0 3" | sudo tee -a /etc/fstab
+systemctl daemon-reload
 
 # yay; plex-media-server spotify jetbrains-toolbox
