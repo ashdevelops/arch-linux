bc67f61a3b424395d44fc87aa49294732060ecd5
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-26 16:18:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-sudo pacman -Syu ntfs-3g git nano rofi polybar plex-media-server spotify dunst blueman jetbrains-toolbox kitty
+sudo pacman -Syu ntfs-3g git nano rofi polybar dunst bluez bluez-utils kitty
 
 echo "UUID=46BA9DEABA9DD6B1       /mnt/other-drive	ntfs	defaults,noatime,rw,nofail 0 3" | sudo tee -a /etc/fstab
 
-# TODO: Restore config files
+# yay; plex-media-server spotify jetbrains-toolbox
