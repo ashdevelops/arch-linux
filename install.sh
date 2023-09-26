7ec77930693c44c20bff9de8a6cc7a9c755d6228
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-26 17:44:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-sudo pacman -Syu base-devel xorg-server xorg-xinit i3-wm sddm ntfs-3g git nano rofi polybar dunst blueman bluez bluez-utils kitty nautilus nitrogen firefox picom neofetch
+sudo pacman -Syu base-devel xorg-server xorg-xinit i3-wm sddm ntfs-3g git nano rofi polybar dunst blueman bluez bluez-utils kitty nautilus nitrogen firefox picom neofetch --noconfirm
 
 sudo systemctl enable --now bluetooth.service
-bluetoothctl scan on
+
 echo "xrandr --output HDMI-1 --right-of DP-1" | sudo tee -a /etc/X11/xinit/xinitrc.d/51-display-settings.sh
 echo "nitrogen --set-zoom --random ~/wallpapers --head=0 && nitrogen --set-zoom-fill --random ~/wallpapers --head=1" | sudo tee -a /etc/X11/xinit/xinitrc.d/52-wallpaper-settings.sh
 
@@ -12,12 +12,12 @@ echo "UUID=46BA9DEABA9DD6B1       /mnt/other-drive	ntfs	defaults,noatime,rw,nofa
 systemctl daemon-reload
 
 sudo pacman -S --needed base-devel git && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-yay -Syu betterlockscreen plex-media-server spotify jetbrains-toolbox visual-studio-code-bin php composer
+yay -Syu betterlockscreen plex-media-server spotify jetbrains-toolbox visual-studio-code-bin php composer --nocleanmenu --nodiffmenu
 
 cp -a config/. ~/.config
 
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
-sed -n '/twm &/q;p' ~/.xinitrc
+sed -n '/twm &/q;p' ~/.xinitrc | tee ~/.xinitrc
 echo "exec i3" > ~/.xinitrc
 
 mkdir ~/scripts
