e1104d313bf3067b31de5df8dee2756de02d7f6f
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-27 17:39:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,15 @@ echo "nitrogen --set-zoom --random ~/wallpapers --head=0 && nitrogen --set-zoom-
 sudo chmod +x /etc/X11/xinit/xinitrc.d/51-display-settings.sh
 sudo chmod +x /etc/X11/xinit/xinitrc.d/52-wallpaper-settings.sh
 
+cp -a config/. /home/ash/.config/
+cp .xinitrc /home/ash/.xinitrc
+cp -R wallpapers/ /home/ash/wallpapers
+
 echo "UUID=46BA9DEABA9DD6B1       /mnt/other-drive	ntfs	defaults,noatime,rw,nofail 0 3" | sudo tee -a /etc/fstab
 systemctl daemon-reload
 
 sudo pacman -S --needed base-devel git && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 yay -Syu betterlockscreen plex-media-server spotify jetbrains-toolbox visual-studio-code-bin php composer --nocleanmenu --nodiffmenu --noconfirm
-
-cp -a config/. ~/.config/
-cp ./.xinitrc ~/.xinitrc
-cp -R wallpapers/ ~/wallpapers
 
 mkdir ~/scripts
 
