e5836943551e7372a99a881af2655561da43d54c
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-28 08:48:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@ sudo pacman -Syu base-devel xorg-server xorg-xinit i3-wm ntfs-3g git nano rofi p
 
 echo "xrandr --output HDMI-1 --left-of DP-1" | sudo tee -a /etc/X11/xinit/xinitrc.d/51-display-settings.sh
 echo "nitrogen --set-zoom --random ~/wallpapers --head=0 && nitrogen --set-zoom-fill --random ~/wallpapers --head=1" | sudo tee -a /etc/X11/xinit/xinitrc.d/52-wallpaper-settings.sh
+echo "export PATH=\"\$HOME/.config/composer/vendor/bin:\$PATH\"" | sudo tee -a /etc/X11/xinit/xinitrc.d/53-environment-variables.sh
 
-sudo chmod +x /etc/X11/xinit/xinitrc.d/51-display-settings.sh
-sudo chmod +x /etc/X11/xinit/xinitrc.d/52-wallpaper-settings.sh
+sudo chmod +x /etc/X11/xinit/xinitrc.d/*.sh
 
 cp -a config/. /home/ash/.config/
 cp .xinitrc /home/ash/.xinitrc
