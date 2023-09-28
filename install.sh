c49ceff9e2d962b19f5b4f975a3b34d550ce11b2
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-28 20:45:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syu base-devel xorg-server xorg-xinit i3-wm ntfs-3g git nano rofi polybar dunst blueman bluez bluez-utils kitty nautilus nitrogen firefox picom neofetch dotnet-runtime dotnet-sdk ranger pulsemixer libpulse htop flameshot sddm gnome-keyring --noconfirm
+sudo pacman -Syu base base-devel xorg-server xorg-xinit i3-wm ntfs-3g git nano rofi polybar dunst blueman bluez bluez-utils kitty nautilus nitrogen firefox picom neofetch dotnet-runtime dotnet-sdk ranger pulsemixer libpulse htop flameshot sddm gnome-keyring --noconfirm
 
 echo "xrandr --output HDMI-1 --left-of DP-1" | sudo tee -a /etc/X11/xinit/xinitrc.d/51-display-settings.sh
 echo "nitrogen --set-zoom --random ~/wallpapers --head=0 && nitrogen --set-zoom-fill --random ~/wallpapers --head=1" | sudo tee -a /etc/X11/xinit/xinitrc.d/52-wallpaper-settings.sh
