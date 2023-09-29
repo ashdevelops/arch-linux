a65f61276eadc0c45b8814500ecb2d27612dac0a
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-29 14:44:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-sudo pacman -Syu --noconfirm - < pkglist > /dev/null 2>&1
-    
+sudo pacman -Syu --noconfirm - < pkglist
+
 echo "xrandr --output HDMI-1 --left-of DP-1" | sudo tee -a /etc/X11/xinit/xinitrc.d/51-display-settings.sh
 echo "nitrogen --set-zoom --random ~/wallpapers --head=0 && nitrogen --set-zoom-fill --random ~/wallpapers --head=1" | sudo tee -a /etc/X11/xinit/xinitrc.d/52-wallpaper-settings.sh
 echo "export PATH=\"\$HOME/.config/composer/vendor/bin:\$PATH\"" | sudo tee -a /etc/X11/xinit/xinitrc.d/53-environment-variables.sh
