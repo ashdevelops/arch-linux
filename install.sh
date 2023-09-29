a2621a67f8887535747d791a86d8022873d51d88
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-29 14:05:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-sudo pacman -Syu base base-devel xorg-server xorg-xinit i3-wm ntfs-3g git nano rofi polybar dunst blueman bluez bluez-utils kitty nautilus firefox neofetch htop discord flameshot gnome-keyring nitrogen ranger dotnet-runtime dotnet-sdk sddm libpulse pipewire pipewire-pulse --noconfirm
-
+sudo pacman -Syu - < pkglist
+    
 echo "xrandr --output HDMI-1 --left-of DP-1" | sudo tee -a /etc/X11/xinit/xinitrc.d/51-display-settings.sh
 echo "nitrogen --set-zoom --random ~/wallpapers --head=0 && nitrogen --set-zoom-fill --random ~/wallpapers --head=1" | sudo tee -a /etc/X11/xinit/xinitrc.d/52-wallpaper-settings.sh
 echo "export PATH=\"\$HOME/.config/composer/vendor/bin:\$PATH\"" | sudo tee -a /etc/X11/xinit/xinitrc.d/53-environment-variables.sh
