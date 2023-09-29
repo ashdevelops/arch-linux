42762bc422e8413bddf88cd277fdf666244a06ea
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-29 08:18:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-sudo pacman -Syu base base-devel xorg-server xorg-xinit i3-wm ntfs-3g git nano rofi polybar dunst blueman bluez bluez-utils kitty nautilus nitrogen firefox picom neofetch dotnet-runtime dotnet-sdk ranger pulsemixer libpulse htop flameshot sddm gnome-keyring discord --noconfirm
+sudo pacman -Syu base base-devel xorg-server xorg-xinit i3-wm ntfs-3g git nano rofi polybar dunst blueman bluez bluez-utils kitty nautilus firefox neofetch htop discord --noconfirm
+# non-essential: sddm nitrogen picom dotnet-runtime dotnet-sdk ranger pulse-mixer libpulse flameshot gnome-keyring
 
 echo "xrandr --output HDMI-1 --left-of DP-1" | sudo tee -a /etc/X11/xinit/xinitrc.d/51-display-settings.sh
 echo "nitrogen --set-zoom --random ~/wallpapers --head=0 && nitrogen --set-zoom-fill --random ~/wallpapers --head=1" | sudo tee -a /etc/X11/xinit/xinitrc.d/52-wallpaper-settings.sh
@@ -15,13 +16,13 @@ sudo mkdir /mnt/other-drive
 echo "UUID=46BA9DEABA9DD6B1       /mnt/other-drive	ntfs	defaults,noatime,rw,nofail 0 3" | sudo tee -a /etc/fstab
 systemctl daemon-reload
 
-sudo pacman -S --needed base-devel git && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-yay -Syu betterlockscreen plex-media-server spotify visual-studio-code-bin php composer --nocleanmenu --nodiffmenu --noconfirm
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+yay -Syu jetbrains-toolbox php composer spotify betterlockscreen plex-media-server visual-studio-code-bin --nocleanmenu --nodiffmenu --noconfirm
 
 mkdir ~/scripts
 
 sudo systemctl enable --now bluetooth.service
-sudo systemctl enable --now sddm.service
+#sudo systemctl enable --now sddm.service
 sudo systemctl enable --now plexmediaserver
 
 reboot
