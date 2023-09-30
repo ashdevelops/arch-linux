8a2a74bcaf5e159fe2868dfdc7b0bbb69dc18699
ash <51748995+ashdevelops@users.noreply.github.com>
2023-09-30 14:17:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,14 +16,13 @@ systemctl daemon-reload
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 yay -Syu jetbrains-toolbox php composer spotify plex-media-server visual-studio-code-bin zsh vlc waybar-git swaylock-effects hyprpaper --nocleanmenu --nodiffmenu --noconfirm
 
-sudo systemctl enable --now bluetooth.service
-sudo systemctl enable --now plexmediaserver
-sudo systemctl enable sddm.service
-
 timedatectl set-timezone Europe/London
 sudo usermod -a -G input $USER
 
 sh -c "$(curl -fsSL raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 echo "neofetch" >> ~/.zshrc
 
-echo "Script finished - suggesting you reboot..."
+sudo systemctl enable --now bluetooth.service
+sudo systemctl enable --now plexmediaserver
+sudo systemctl enable --now sddm.service
